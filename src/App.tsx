d4c733c37ad377e18ace4e2e77425ce1e47e9e72
okadahideya <hideya400@gmail.com>
2024-11-02 21:36:11 +0900

    完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,125 +1,172 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Report from './pages/Report';
-import NoMatch from './pages/NoMatch';
-import Applayout from './components/layout/Applayout';
-import  {theme } from './theme/theme'
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
-import { Transaction } from './types/index';
-import { addDoc, collection, deleteDoc, doc, getDocs, updateDoc } from "firebase/firestore";
-import { db } from './components/firebase';
-import { formatMonth } from './utils/formatting';
-import { Schema, transactionSchema } from './validations/schema';
+import React, { useState } from "react";
 
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Report from "./pages/Report";
+import NoMatch from "./pages/NoMatch";
+import AppLayout from "./components/layout/Applayout";
+import { theme } from "./theme/theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
+import { Schema } from "./validations/schema";
+import { db } from './components/firebase';
+import { formatMonth } from "./utils/formatting";
+import { Transaction } from "./types";
 
 function App() {
-  // エラー判別
-  function isFireSotreError(err: unknown):err is {code: string, message: string} {
-    return typeof err === "object" && err != null && "code" in err
-  }
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [isLoading, setIsLoading] = useState(true);
 
-  // firebase取得
-  useEffect(() => {
-    const fetchTransactions = async() => {
+  //型ガード
+  function isFirestoreError(
+    err: unknown
+  ): err is { code: string; message: string } {
+    return typeof err === "object" && err !== null && "code" in err;
+  }
+
+  //firestoreのデータを全て取得
+  React.useEffect(() => {
+    const fecheTransactions = async () => {
       try {
         const querySnapshot = await getDocs(collection(db, "Transactios"));
         const transactionsData = querySnapshot.docs.map((doc) => {
           return {
             ...doc.data(),
             id: doc.id,
-          } as Transaction
+          } as Transaction;
         });
-
         setTransactions(transactionsData);
       } catch (err) {
-        if(isFireSotreError(err)) {
-          console.error("Firebaseエラー",err)
+        if (isFirestoreError(err)) {
+          console.error("firestoreのエラーは：", err);
         } else {
-          console.error(err)
+          console.error("一般的なエラーは:", err);
         }
+      } finally {
+        setIsLoading(false);
       }
-    }
-    fetchTransactions();
+    };
+    fecheTransactions();
   }, []);
 
-  //一月分のデータ
-  const monthlyTransactions = transactions.filter((transaction) => {
-    return transaction.date.startsWith(formatMonth(currentMonth));
-  })
-
+  //取引を保存する処理
   const handleSaveTransaction = async (transaction: Schema) => {
     try {
-      // firebaseにデータ保存
+      //firestoreにデータを保存
       const docRef = await addDoc(collection(db, "Transactios"), transaction);
-      console.log("Document written with ID: ", docRef.id);
-
       const newTransaction = {
         id: docRef.id,
-        ...transaction
+        ...transaction,
       } as Transaction;
-
-      setTransactions([...transactions, newTransaction])
-    } catch(err) {
-      if(isFireSotreError(err)) {
-        console.error("Firebaseエラー",err)
-      } else {
-        console.error(err)
-      }
-    }
-  }
-
-  const handleDeleteTransaction = async (transactionId: string) => {
-    try {
-      await deleteDoc(doc(db, "Transactios", transactionId));
-      const filteredTransactions = transactions.filter((transaction) => transaction.id !== transactionId);
-      setTransactions(filteredTransactions);
+      setTransactions((prevTransaction) => [
+        ...prevTransaction,
+        newTransaction,
+      ]);
     } catch (err) {
-      if(isFireSotreError(err)) {
-        console.error("Firebaseエラー",err)
+      if (isFirestoreError(err)) {
+        console.error("firestoreのエラーは：", err);
       } else {
-        console.error(err)
+        console.error("一般的なエラーは:", err);
       }
     }
-  }
+  };
 
-  const handleUpdateTransaction = async (transaction: Schema, transactionId: string) => {
+  //削除処理
+  const handleDeleteTransaction = async (
+    transactionIds: string | readonly string[]
+  ) => {
     try {
+      const idsToDelete = Array.isArray(transactionIds)
+        ? transactionIds
+        : [transactionIds];
+
+      for (const id of idsToDelete) {
+        //firestoreのデータ削除
+        await deleteDoc(doc(db, "Transactios", id));
+      }
+      //複数の取引を削除可能
+      const filterdTransactions = transactions.filter(
+        (transaction) => !idsToDelete.includes(transaction.id)
+      );
+      setTransactions(filterdTransactions);
+    } catch (err) {
+      if (isFirestoreError(err)) {
+        console.error("firestoreのエラーは：", err);
+      } else {
+        console.error("一般的なエラーは:", err);
+      }
+    }
+  };
+
+  //更新処理
+  const handleUpdateTransaction = async (
+    transaction: Schema,
+    transactionId: string
+  ) => {
+    try {
+      //firestore更新処理
       const docRef = doc(db, "Transactios", transactionId);
-      await updateDoc(docRef, transaction);
 
-      // フロント更新処理
-      const updateTransactions = transactions.map((t) => t.id === transactionId ? {...t, ...transaction} : t) as Transaction[];
-      setTransactions(updateTransactions);
+      await updateDoc(docRef, transaction);
+      //フロント更新
+      const updatedTransactions = transactions.map((t) =>
+        t.id === transactionId ? { ...t, ...transaction } : t
+      ) as Transaction[];
+      setTransactions(updatedTransactions);
     } catch (err) {
-      if(isFireSotreError(err)) {
-        console.error("Firebaseエラー",err)
+      if (isFirestoreError(err)) {
+        console.error("firestoreのエラーは：", err);
       } else {
-        console.error(err)
+        console.error("一般的なエラーは:", err);
       }
     }
-  }
+  };
+
+  //月間の取引データを取得
+  const monthlyTransactions = transactions.filter((transaction) => {
+    return transaction.date.startsWith(formatMonth(currentMonth));
+  });
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
         <Routes>
-          <Route path="/" element={<Applayout />}>
-            <Route index element={
-              <Home 
-                monthlyTransactions={monthlyTransactions} 
-                setCurrentMonth={setCurrentMonth}
-                onSaveTransaction={handleSaveTransaction}
-                onDeleteTransaction={handleDeleteTransaction}
-                onUpdateTransaction={handleUpdateTransaction}
-              />} 
+          <Route path="/" element={<AppLayout />}>
+            <Route
+              index
+              element={
+                <Home
+                  monthlyTransactions={monthlyTransactions}
+                  setCurrentMonth={setCurrentMonth}
+                  onSaveTransaction={handleSaveTransaction}
+                  onDeleteTransaction={handleDeleteTransaction}
+                  onUpdateTransaction={handleUpdateTransaction}
+                />
+              }
             />
-            <Route path="/report" element={<Report />} />
+            <Route
+              path="/report"
+              element={
+                <Report
+                  monthlyTransactions={monthlyTransactions}
+                  setCurrentMonth={setCurrentMonth}
+                  currentMonth={currentMonth}
+                  isLoading={isLoading}
+                  onDeleteTransaction={handleDeleteTransaction}
+                />
+              }
+            />
             <Route path="*" element={<NoMatch />} />
           </Route>
         </Routes>
--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,8 +1,69 @@
+import { Grid, Paper } from '@mui/material'
 import React from 'react'
+import MonthSelector from '../components/MonthSelector'
+import CategoryChart from '../components/CategoryChart'
+import BarChart from '../components/BarChart'
+import TransactionTable from '../components/TransactionTable'
+import { Transaction } from '../types'
 
-const Report = () => {
+interface ReportProps {
+  monthlyTransactions: Transaction[];
+  currentMonth: Date;
+  setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>; //追加
+  onDeleteTransaction: (ids: string | readonly string[]) => Promise<void>;
+  isLoading: boolean;
+}
+
+
+const Report = ({ 
+  monthlyTransactions,
+  currentMonth,
+  setCurrentMonth,
+  onDeleteTransaction,
+  isLoading,
+}: ReportProps ) => {
+  const commonPaperStyle = {
+    height: "400",
+    display: "flex",
+    flexDirection: "column"
+  }
   return (
-    <div>Report</div>
+    <Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          {/* 日付選択エリア */}
+          <MonthSelector 
+            currentMonth={currentMonth}
+            setCurrentMonth={setCurrentMonth}
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Paper sx={commonPaperStyle}>
+            {/* 円グラフ */}
+            <CategoryChart 
+              monthlyTransactions={monthlyTransactions}
+              isLoading={isLoading}
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Paper sx={commonPaperStyle}>
+            {/* 棒グラフ */}
+            <BarChart
+              monthlyTransactions={monthlyTransactions}
+              isLoading={isLoading}
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          {/* テーブル */}
+          <TransactionTable 
+            monthlyTransactions={monthlyTransactions}
+            onDeleteTransaction={onDeleteTransaction}
+          />
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
